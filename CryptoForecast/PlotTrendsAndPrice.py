7d810c0d83bd695a6095a3198d537aeff612e25c
7yl4r <murray.tylar@gmail.com>
2017-06-08 23:17:35 -0400

    co-plot working thanks to matplotlib direct calls

--- CryptoForecast/PlotTrendsAndPrice.py
+++ CryptoForecast/PlotTrendsAndPrice.py
@@ -27,10 +27,19 @@ class PlotTrendsAndPrice(luigi.Task):
         return luigi.LocalTarget(config.plot_dir+"trends.png")
 
     def run(self):
-        trends_dta = pandas.read_csv(self.input()[0].path)
-        price_dta = pandas.read_csv(self.input()[1].path)
-        ax = trends_dta.plot(figsize=config.fig_size)
-        price_dta.plot(ax=ax, figsize=config.fig_size)
+        trends_dta = pandas.read_csv(self.input()[0].path, index_col=0)
+        price_dta = pandas.read_csv(self.input()[1].path, index_col=0)
+
+        plt.plot(
+            [datetime.datetime.strptime(d, '%Y-%m-%d') for d in trends_dta.index],
+            [price_dta.price.max()/100.0 * v for v in trends_dta.bitcoin]  # scale trends percentage
+        )
+        plt.plot(
+            [datetime.datetime.strptime(d, '%Y-%m-%d') for d in price_dta.index],
+            price_dta.price
+        )
+        # ax = trends_dta.plot(figsize=config.fig_size, x='Date', y='bitcoin', sharex=True)
+        # price_dta.plot(ax=ax, figsize=config.fig_size, x='Date', y='price', sharex=True)
 
         print("\nsaving plot to ", self.output().path, "...\n")
         plt.savefig(self.output().path, bbox_inches='tight')
