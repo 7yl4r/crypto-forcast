d8baee446181676b4092e3ac8e35c1e70654d1a9
Max Isom <codetheweb@icloud.com>
2018-06-01 21:08:49 -0500

    Fix behavior for appending to existing file

--- CryptoForecast/eth/IngestPricesRealtime.py
+++ CryptoForecast/eth/IngestPricesRealtime.py
@@ -33,23 +33,18 @@ class IngestPricesRealtimeETH(luigi.Task):
             lastTrades = pd.read_csv(self.output().path)
             lastTrades['time'] = pd.to_datetime(lastTrades['time'])
 
-            # get new trades
-            lastTradeId = lastTrades.tail(n=1).values[0][0]
-            print(lastTradeId)
-            trades = pd.DataFrame(client.get_historical_trades(symbol=tradingPair, fromId=(lastTradeId + 1)))
-        else:
-            # old data doesn't exist
-            trades = pd.DataFrame(client.get_historical_trades(symbol=tradingPair))
+        # get new trades
+        trades = pd.DataFrame(client.get_historical_trades(symbol=tradingPair))
 
         # convert columns
         trades['time'] = pd.to_datetime(trades['time'], unit='ms', origin='unix')
         trades['price'] = pd.to_numeric(trades['price'])
 
         # remove irrelevant columns
-        trades = trades[['id', 'time','price',]]
+        trades = trades[['id', 'time','price']]
 
         if ('lastTrades' in vars()):
-            combinedTrades = pd.concat([lastTrades, trades])
+            combinedTrades = pd.concat([lastTrades, trades]).drop_duplicates(subset='id')
             combinedTrades.to_csv(self.output().path, index=False)
         else:
             trades.to_csv(self.output().path, index=False)
