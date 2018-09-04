212700f059b37e356e6dbd36911f43e827259ac9
7yl4r <murray.tylar@gmail.com>
2018-09-03 22:15:19 -0400

    flake8 compliance

--- CryptoForecast/trade/plotters/Bollinger.py
+++ CryptoForecast/trade/plotters/Bollinger.py
@@ -5,10 +5,10 @@ Outputs Bollinger trade data to plot
 import luigi
 import pandas
 import matplotlib.pyplot as plt
-import datetime
 
 import config
 from trade.strategy.BollingerBands import BollingerBands
+
 
 class PlotBollinger(luigi.Task):
     def requires(self):
@@ -20,15 +20,17 @@ class PlotBollinger(luigi.Task):
     def run(self):
         print("\nsaving plot to ", self.output().path, "...\n")
         dta = pandas.read_csv(
-          self.input()[0]["bollinger"].path,
-          parse_dates=['Date(UTC)'],
-          converters={'Value': float},
+            self.input()[0]["bollinger"].path,
+            parse_dates=['Date(UTC)'],
+            converters={'Value': float},
         )
         print(dta.info())
 
         dta.plot(x='Date(UTC)', y='Value')
 
-        dta[['Value', 'EMA', 'STD', 'Upper Band', 'Lower Band']].plot(figsize=config.fig_size)
+        dta[['Value', 'EMA', 'STD', 'Upper Band', 'Lower Band']].plot(
+            figsize=config.fig_size
+        )
         plt.title('Bollinger Band for ETH/BTC')
         plt.ylabel('ETH/BTC Exchange Rate')
 
