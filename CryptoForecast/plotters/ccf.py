f407927f3ca6b1de833eabcacd49bb6540ba4e47
7yl4r <murray.tylar@gmail.com>
2019-11-10 01:24:25 -0500

    flake8 compliance

--- CryptoForecast/plotters/ccf.py
+++ CryptoForecast/plotters/ccf.py
@@ -1,9 +1,12 @@
 """
+!!! moved to plo7y/plotters/ccf.py !!!
+
 cross-correlations and partial cross-correlations
 """
 
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
+
 
 def plotCCF(dta, exog, saveFigPath, **kwargs):
     zoomLagView = 120   # max lag of interest (for zoomed view)
@@ -12,23 +15,23 @@ def plotCCF(dta, exog, saveFigPath, **kwargs):
     kwargs.setdefault('markersize', 5)
     kwargs.setdefault('linestyle', 'None')
 
-    fig = plt.figure(figsize=(12,8))
-    ax1=fig.add_subplot(211)
+    fig = plt.figure(figsize=(12, 8))
+    ax1 = fig.add_subplot(211)
 
     ax1.set_ylabel('CCF')
     ax1.set_xlabel('lag')
     # print dta
 
     print("plotting CCF...")
-    print('SIZES:',len(dta.values.squeeze()), ',', len(exog.values.squeeze()))
+    print('SIZES:', len(dta.values.squeeze()), ',', len(exog.values.squeeze()))
 
     ccf_x = sm.tsa.ccf(dta.values.squeeze(), exog.values.squeeze())
-    ax1.plot(range(1,len(ccf_x)+1), ccf_x, **kwargs)
+    ax1.plot(range(1, len(ccf_x)+1), ccf_x, **kwargs)
 
-    ax2=fig.add_subplot(212)
-    ax2.plot(range(1,zoomLagView+1), ccf_x[:zoomLagView], **kwargs)
+    ax2 = fig.add_subplot(212)
+    ax2.plot(range(1, zoomLagView+1), ccf_x[:zoomLagView], **kwargs)
 
-    if (saveFigPath==None):
+    if (saveFigPath is None):
         plt.show()
     else:
         plt.savefig(str(saveFigPath), bbox_inches='tight')
